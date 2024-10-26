adc54290936e0a67e64e3cf55202e1f04aef6d30
Duarte O.Carmo <duarteocarmo@Duartes-MacBook-Pro.local>
2024-10-26 15:04:36 +0200

    update dots

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -36,7 +36,6 @@ alias pd="exit"
 
 set PATH /usr/local/bin $PATH
 thefuck --alias | source
-source ~/.asdf/asdf.fish
 
 
 set -gx ATUIN_NOBIND "true"
@@ -45,3 +44,6 @@ atuin init fish | source
 # bind to ctrl-r in normal and insert mode, add any other bindings you want here too
 bind \cr _atuin_search
 bind -M insert \cr _atuin_search
+
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+uvx --generate-shell-completion fish | source
