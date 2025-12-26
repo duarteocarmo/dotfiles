83f49f6ea2e18e1e9af52d3c15a7b979dd588c66
duarteocarmo <me@duarteocarmo.com>
2025-12-26 19:12:04 +0000

    Update minimal config

--- .config/nvim-minimal/init.lua
+++ .config/nvim-minimal/init.lua
@@ -22,7 +22,6 @@ o.shell = "/opt/homebrew/bin/fish"
 o.completeopt = { "menuone", "noselect", "popup" }
 o.wildmode = { "lastused", "full" }
 o.pumheight = 15
-o.laststatus = 0
 o.winborder = "rounded"
 o.undofile = true
 o.ignorecase = true
@@ -46,26 +45,38 @@ map("n", "<leader>y", function() -- copy relative filepath to clipboard
 end)
 
 map("n", "<leader>ff", "<cmd>Pick files<cr>")
+map("n", "<leader>rr", "<cmd>:restart<cr>")
 map("n", "<leader>fg", "<cmd>Pick grep_live<cr>")
+map("n", "<leader>gg", "<cmd>:LazyGit<cr>")
+map({ "n", "v" }, "<leader>pc", ":GpChatNew popup<CR>", opts)
+map({ "n", "v" }, "<leader>pr", ":GpRewrite<CR>", opts)
+map({ "n", "v" }, "<leader>pa", ":GpAppend<CR>", opts)
 
 map({ "n", "v" }, "<C-k>", function()
 	require("conform").format({ async = false, lsp_fallback = true })
 end, opts)
 
-vim.pack.add({
-	"https://github.com//mason-org/mason.nvim",
-	"https://github.com//neovim/nvim-lspconfig",
-	"https://github.com/mason-org/mason-lspconfig.nvim",
-	"https://github.com/numToStr/FTerm.nvim",
-	"https://github.com/nvim-mini/mini.completion",
-	"https://github.com/nvim-mini/mini.pick",
-	"https://github.com/nvim-mini/mini.diff",
-	"https://github.com/nvim-treesitter/nvim-treesitter",
-	"https://github.com/sindrets/diffview.nvim",
-	"https://github.com/stevearc/conform.nvim",
-	"https://github.com/tpope/vim-fugitive",
-	"https://github.com/tpope/vim-rhubarb",
-})
+local plugins = {
+	"mason-org/mason.nvim",
+	"neovim/nvim-lspconfig",
+	"mason-org/mason-lspconfig.nvim",
+	"numToStr/FTerm.nvim",
+	"nvim-mini/mini.nvim",
+	"nvim-treesitter/nvim-treesitter",
+	"sindrets/diffview.nvim",
+	"stevearc/conform.nvim",
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
+	"zbirenbaum/copilot.lua",
+	"copilotlsp-nvim/copilot-lsp",
+	"rachartier/tiny-inline-diagnostic.nvim",
+	"kdheepak/lazygit.nvim",
+	"robitx/gp.nvim",
+}
+
+vim.pack.add(vim.tbl_map(function(repo)
+	return "https://github.com/" .. repo
+end, plugins))
 
 vim.cmd("colorscheme default")
 
@@ -75,7 +86,36 @@ require("mason").setup()
 require("mason-lspconfig").setup({ ensure_installed = { "lua_ls", "rust_analyzer" } })
 require("mini.completion").setup()
 require("mini.pick").setup()
+require("mini.statusline").setup({})
 require("mini.diff").setup()
+require("tiny-inline-diagnostic").setup()
+require("copilot").setup({
+	nes = {
+		enabled = false, -- Not nice.
+		keymap = {
+			accept_and_goto = "<leader>p",
+			accept = false,
+			dismiss = "<Esc>",
+		},
+	},
+	suggestion = {
+		enabled = true,
+		auto_trigger = true,
+		keymap = {
+			accept = "<C-J>",
+		},
+	},
+	filetypes = {
+		["*"] = true,
+		beancount = false,
+		sh = function()
+			if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env") then
+				return false
+			end
+			return true
+		end,
+	},
+})
 require("nvim-treesitter").install({ "lua", "rust", "python" })
 require("FTerm").setup({
 	border = "single",
@@ -102,3 +142,46 @@ require("conform").setup({
 		lua = { "stylua" },
 	},
 })
+local code_prompt = assert(io.open(os.getenv("HOME") .. "/.AGENTS.MD", "r")):read("*all")
+require("gp").setup({
+	default_command_agent = "gpt-5-mini",
+	default_chat_agent = "gpt-5-mini",
+	providers = {
+		copilot = {
+			disable = false,
+			endpoint = "https://api.githubcopilot.com/chat/completions",
+			secret = {
+				"bash",
+				"-c",
+				"cat ~/.config/github-copilot/apps.json | sed -e 's/.*oauth_token...//;s/\".*//'",
+			},
+		},
+	},
+	agents = {
+		{
+			provider = "copilot",
+			name = "Copilot - claude-sonnet-4.5",
+			chat = true,
+			command = true,
+			model = { model = "claude-sonnet-4.5" },
+			system_prompt = code_prompt,
+		},
+		{
+			provider = "copilot",
+			name = "Copilot - gpt-5-mini",
+			chat = true,
+			command = true,
+			model = { model = "gpt-5-mini" },
+			system_prompt = code_prompt,
+		},
+	},
+})
+vim.lsp.config.beancount = {
+	cmd = { "beancount-language-server", "--stdio" },
+	filetypes = { "beancount" },
+	root_markers = { ".git" },
+	settings = {
+		journal_file = os.getenv("HOME") .. "/Repos/accounting/duarte.beancount",
+	},
+}
+vim.lsp.enable("beancount")
